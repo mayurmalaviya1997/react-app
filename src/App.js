8a82d465cef01fe1b6d3845656ce7995ceb067ae
mayurmalaviya1997 <mmalaviya517@gmail.com>
2023-05-01 23:05:15 +0530

    mayur first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
-      mayur
+      <Home/>
+      <About/>
     </div>
   );
 }
